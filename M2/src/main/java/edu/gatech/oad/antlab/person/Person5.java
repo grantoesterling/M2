084087444195ba1e6b6c1219c4461a55da15bbf8
jessieprice <jprice65@gatech.edu>
2018-01-30 20:10:39 -0500

    implement method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Jessie
  *  @version 1.1
  */
 public class Person5 {
@@ -30,8 +30,15 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+	    char[] newName = new char[];
+	  	for (int i = 2, j = 0; i < input.length() && j < input.length() - 2; i++, j++) {
+	  		newName[i] = input.charAt(j);
+		}
+		for (int i = 2, j = newName.length; i > 0 && j > newName.length - 2; i--, j--) {
+	  		newName[j] = input.charAt(i);
+		}
+		//Person 5 put your implementation here
+	  return java.util.Arrays.toString(newName);
 	}
 	
 	/**
