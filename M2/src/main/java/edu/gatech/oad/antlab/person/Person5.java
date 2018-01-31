e1eb7800b30d50a06097ad990c7c52cfd6d902cd
grantoesterling <grantoesterling1@gmail.com>
2018-01-30 23:04:28 -0500

    Update Person5.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,7 +30,7 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	    	String begin = input.substring(2, input.length());
+	    	String begin = input.substring(1, input.length());
 		String last = input.substring(0,1);
 		return begin.concat(last);
 	}
